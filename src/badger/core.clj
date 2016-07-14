2be50338d2e65426d3a13d8a53183df33c975ca8
Ken Shih <ken@dhcpb249.dev.hq.meetup.com>
2016-07-14 12:58:24 -0400

    pretty output in slack

--- src/badger/core.clj
+++ src/badger/core.clj
@@ -10,12 +10,17 @@
 (defn handler [request]
   {:status 200
     :headers {"Content-Type" "application/json"}
-    :body "{\"response_type\": \"ephemeral\",\"text\": \"Here are the currently open tickets:\" }"
+    :body (str "{\"response_type\": \"in_channel\",\"text\": \"Here are the top Badgers!\" "
+    ",\"attachments\": [{\"fallback\": \"Coverage Cueen\", \"color\": \"#36a64f\", \"title\": \"Coverage Cueen\",\"footer\": \"covered so much code, the code could sleep well\",\"footer_icon\": \"http://www.gummyworm.net/wp-content/uploads/2015/02/Pinguino-png-129x129.png\"},{\"fallback\": \"Required plain-text summary of the attachment.\", \"color\": \"#36a64f\", \"title\": \"Hackathon winner\",\"footer\": \"no one can ever take that from ya!\",\"footer_icon\": \"https://platform.slack-edge.com/img/default_application_icon.png\"}]"
+    "}"
+    )
   })
+
+; \"attachments\": [{\"fallback\": \"Coverage Cueen\", \"color\": \"#36a64f\", \"title\": \"Coverage Cueen\",\"footer\": \"covered so much code, the code could sleep well\",\"footer_icon\": \"http://www.gummyworm.net/wp-content/uploads/2015/02/Pinguino-png-129x129.png\"},{\"fallback\": \"Required plain-text summary of the attachment.\", \"color\": \"#36a64f\", \"title\": \"Hackathon winner\",\"footer\": \"no one can ever take that from ya!\",\"footer_icon\": \"https://platform.slack-edge.com/img/default_application_icon.png\"}]
 
 (defn -main
   "starts web application"
   [& args]
   (println "starting app")
-  (run-jetty handler {:port 80})
+  (run-jetty handler {:port 3000})
   )
