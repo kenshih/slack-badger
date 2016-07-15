5dfd81c9225b21e511964b92b01c36d6787f90bc
Sak Lee <sak@meetup.com>
2016-07-14 21:50:15 -0400

    result file name does not need path

--- src/badger/badgerer.clj
+++ src/badger/badgerer.clj
@@ -7,7 +7,7 @@
 (defn add-badge
   "receives profile pic filename and badge number, and saves the badged pic and return the filename"
   [profile-filename badge-num]
-  (let [result-filename (prefix-path (clojure.string/replace profile-filename #"\." (str "_" badge-num ".")))
+  (let [result-filename (clojure.string/replace profile-filename #"\." (str "_" badge-num "."))
         profile-size 600 badge-size 200]
     (q/defsketch badged-profile
       :size [profile-size profile-size]
@@ -17,7 +17,7 @@
                      badge-pos (- profile-size badge-size)]
                  (q/image profile-pic 0 0 profile-size profile-size)
                  (q/image badge-pic badge-pos badge-pos badge-size badge-size)
-                 (q/save result-filename)))
+                 (q/save (prefix-path result-filename))))
       :title "saving to file"
       :draw (fn [] nil))
     (.exit badged-profile)
